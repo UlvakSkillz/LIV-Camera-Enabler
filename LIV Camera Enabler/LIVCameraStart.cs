2f40ca64e036a6a4da1a6ae4376e2168c990d5c1
UlvakSkillz <156770480+UlvakSkillz@users.noreply.github.com>
2024-01-15 23:08:39 -0800

    Initial commit

---
+++ LIV Camera Enabler/LIVCameraStart.cs
@@ -0,0 +1,70 @@
+﻿using MelonLoader;
+using UnityEngine;
+using RUMBLE.Managers;
+using RUMBLE.Players.Subsystems;
+
+namespace LIV_Camera_Enabler
+{
+    public class LIVCameraStart : MelonMod
+    {
+        //variables
+        private string currentScene = "";
+        private bool sceneChanged = false;
+        private bool livInitialized = false;
+        private LIV.SDK.Unity.LIV playerLIV;
+
+        //initializes things
+        public override void OnInitializeMelon()
+        {
+            base.OnInitializeMelon();
+        }
+
+        public override void OnLateInitializeMelon()
+        {
+            base.OnLateInitializeMelon();
+        }
+
+        //run every update
+        public override void OnUpdate()
+        {
+            //normal updates
+            base.OnUpdate();
+            if (sceneChanged)
+            {
+                livInitialized = false;
+                if ((currentScene != "") && (currentScene != "Loader"))
+                {
+                    if (!livInitialized)
+                    {
+                        try
+                        {
+                            playerLIV = GameObject.Find("Game Instance/Initializable/PlayerManager").gameObject.GetComponent<PlayerManager>().localPlayer.Controller.transform.GetChild(8).gameObject.GetComponent<PlayerLIV>().liv;
+                            playerLIV.enabled = true;
+                            livInitialized = true;
+                            MelonLogger.Msg("F10 Menu Camera Enabled");
+                        }
+                        catch
+                        {
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        playerLIV.enabled = true;
+                        MelonLogger.Msg("F10 Menu Camera Enabled");
+                    }
+                }
+                sceneChanged = false;
+            }
+        }
+
+        //called when a scene is loaded
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            base.OnSceneWasLoaded(buildIndex, sceneName);
+            //update current scene
+            currentScene = sceneName;
+            sceneChanged = true;
+        }
+    }
+}
---
+++ LIV Camera Enabler/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using LIV_Camera_Enabler; // The namespace of your mod class
+// ...
+[assembly: MelonInfo(typeof(LIVCameraStart), "LIV Camera Enabler", "1.0.1", "UlvakSkillz")]
+[assembly: MelonGame("Buckethead Entertainment", "RUMBLE")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LIV Camera Enabler")]
+[assembly: AssemblyDescription("Start playing Rumble with the F10 Camera Active")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LIV Camera Enabler")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ce5c6056-f5fe-45d6-92f5-42037a30db8f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
